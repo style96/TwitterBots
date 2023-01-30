ec8b102e5018842557eb436faceec738636493b4
halil <style93@gmail.com>
2023-01-31 01:39:14 +0300

    init comm

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python server.py
+worker: python bots.mentionChecker_stream.py
---
+++ server.py
@@ -0,0 +1,12 @@
+from os import environ
+from flask import Flask
+import bots.mentionChecker_stream
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    mentionChecker_stream.main()
+    return "Tweeting weather and a quote..."
+
+app.run(host='0.0.0.0', port=environ.get('PORT'))
